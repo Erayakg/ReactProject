2360e80825e0f1a44e0faa72d924ecacfe946692
“Eray <90953626+Erayakg@users.noreply.github.com>
2023-02-02 12:10:21 +0300

    changes Postform,post

--- src/components/Post/PostForm.js
+++ src/components/Post/PostForm.js
@@ -1,42 +1,101 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
+import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {  Container } from '@mui/system';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { makeStyles } from '@mui/styles';
+import { InputAdornment, OutlinedInput } from '@mui/material';
+import { json } from 'react-router-dom';
+export default function PostForm(props) {
+    const {UserId,UserName}=props;
+  const [expanded, setExpanded] = React.useState(false);
+  const[text,setText]=React.useState("")
+  const[title,setTitle]=React.useState("")
+ 
+  
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
+  const handleSubmit= ()=>{
+    SavePost();
+  }
+  const handleTitle=(value)=>{
+    setTitle(value);
+  }
+  const handleTxt=(value)=>{
+    setText(value);
+  }
+  const SavePost=()=>{
+    fetch("/post",{
+      method:"POST",
+      headers:{
+        "COntent-Type":"application/json",
+      },
+      body:JSON.stringify({
+      title:title,
+      UserId:52,
+      text:text
 
-export default function BasicCard() {
+      }),
+
+
+
+    }).then((res)=>res.json()).catch((err)=>console.log("error"))
+  }
+
+
+
+
   return (
-    <Card sx={{ minWidth: 275 }}>
+   <Container fixed  >
+    <Card   sx={{ maxWidth: 3450 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            {UserName}
+          </Avatar>
+        }
+        title={<OutlinedInput  
+        id="outlined-adorment-amount"
+        multiline
+        placeholder='Title'
+        inputProps={{ maxLength:25}}
+        fullWidth
+        onChange={(i)=>handleTitle(i.target.value)}
+        >
+        </OutlinedInput>}
+      />
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+        <Typography variant="body2" color="text.secondary">
+        <OutlinedInput  
+        onChange={(i)=>handleTxt(i.target.value)}
+        id="outlined-adorment-amount"
+        multiline
+        placeholder='Text'
+        inputProps={{ maxLength:250}}
+        fullWidth
+        endAdornment={
+          <InputAdornment position='end'>
+          <Button  onClick={handleSubmit} variant="contained" color="success" >save</Button>
+          </InputAdornment>
+        }
+        >
+        </OutlinedInput>
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
+    </Container>
   );
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,7 +2,8 @@
 import Post from "../Post/Post";
 import React,{useState,useEffect} from "react";
 import { makeStyles } from '@mui/styles';
-
+import PostForm from '../Post/PostForm'
+import { Container } from "@mui/system";
 
 function Home(){
     const [error,setError]=useState(null);
@@ -21,7 +22,6 @@ function Home(){
             setError(error);
         }
     )
-        console.log(postlist);
 },[])
 
     if(error){
@@ -30,15 +30,17 @@ function Home(){
     {return   <div>loading</div>}
     else{
 
-        return <div>  
-        
-        <div> {
+        return  <div>
+            <Container fixed >
+            <PostForm  UserId={52} UserName={"eray"}></PostForm>
+            {
             postlist.map(post=>(
                     <Post title={post.title} text={post.txt} UserId={post.userId} UserName={post.userName} ></Post>
             )
-            )
-         }   </div>
-        </div>
+            )}
+            </Container>
+            
+         </div>
     }
 }
 export default Home;
